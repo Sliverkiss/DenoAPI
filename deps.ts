42c907a48f5b2962492ce84fffc32c1178012185
sliverkiss <1393579810@qq.com>
2024-06-14 00:43:10 +0800

    移除依赖

--- deps.ts
+++ deps.ts
@@ -1,4 +1,3 @@
 export * as oak from "https://deno.land/x/oak/mod.ts";
-export * as telegraf from "https://deno.land/x/telegraf/mod.ts";
 export * as fs from "https://deno.land/std/fs/mod.ts";
 export * as path from "https://deno.land/std/path/mod.ts";

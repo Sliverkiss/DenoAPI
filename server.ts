5901cdabdd643e03d2d24cefb0cfeb9f13069a24
sliverkiss <1393579810@qq.com>
2024-06-13 21:57:14 +0800

    打印路径

--- server.ts
+++ server.ts
@@ -3,9 +3,14 @@ import * as router from "file:///src/routes/api.ts";
 
 const app = new Application();
 
+//app.use(async (ctx) => {
+//    await router.routeApi(ctx.request.url.pathname, ctx.request);
+//});
+
 app.use(async (ctx) => {
-    await router.routeApi(ctx.request.url.pathname, ctx.request);
+  ctx.response.body = ctx.request.url.pathname;
 });
 
 console.log("Server is running on http://localhost:8000");
 await app.listen({ port: 8000 });
+

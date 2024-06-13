2a1407604ebecbb8839a3d44fef82628506fb299
sliverkiss <1393579810@qq.com>
2024-06-14 07:21:22 +0800

    动态导入

--- routes.ts
+++ routes.ts
@@ -10,18 +10,21 @@ async function collectRoutes(dir: string, basePath: string = ""): Promise<RouteC
   for await (const entry of fs.walk(dir, { includeDirs: false, followSymlinks: false })) {
     if (entry.isFile && entry.path.endsWith(".ts")) {
       const relativePath = path.relative(Deno.cwd(), entry.path);
-      const modulePath = `./${relativePath}`;
+      const modulePath = `file://${path.resolve(Deno.cwd(), relativePath)}`;
       const { default: fileMethods } = await import(modulePath);
       const routePath = `${basePath}${path.relative(dir, entry.path).replace(".ts", "").replace(/\\/g, "/")}`;
       console.log(routePath);
-      routes[routePath] = fileMethods.default; // 假设所有模块都有一个默认导出
+      routes[routePath] = fileMethods; // 假设所有模块都有一个默认导出
+    } else if (entry.isDirectory) {
+      const subRoutes = await collectRoutes(entry.path, `${basePath}${entry.name}/`);
+      Object.assign(routes, subRoutes);
     }
   }
   return routes;
 }
 
 export async function routeApi(api: string, req: Request): Promise<Response> {
-  const routes = await collectRoutes(path.join(Deno.cwd(), "service"));
+  const routes = await collectRoutes(path.join(Deno.cwd(), "src/service"));
   if (api in routes) {
     return routes[api](req);
   } else {

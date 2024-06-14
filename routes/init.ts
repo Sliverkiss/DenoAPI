7d7ca4f718388ff54b8aa4368babda95a20a76f9
sliverkiss <1393579810@qq.com>
2024-06-15 00:11:57 +0800

    test: 增加主页 v1.0.1(9)

--- routes/init.ts
+++ routes/init.ts
@@ -2,7 +2,7 @@
 import { Context, Response, send, join } from "file:///src/deps.ts";
 
 export async function init(ctx: Context): Promise<Response> {
-    const filePath = join(Deno.cwd(), "public", "index.html");
+    const filePath = '../public/index.html'
     console.log(filePath);
     await send(ctx, filePath);
 }

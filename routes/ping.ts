4ee208b7dcf7931ec0639faa70b49affb08dc52d
sliverkiss <1393579810@qq.com>
2024-06-14 10:23:35 +0800

    fix: 修改ping方法返回值 v1.0.1(5)

--- routes/ping.ts
+++ routes/ping.ts
@@ -1,7 +1,5 @@
 import { Context, Response } from "file:///src/deps.ts";
 
 export async function ping(ctx: Context): Promise<Response> {
-  return new Response(import.meta.url, {
-    status: 200,
-  });
+  return import.meta.url
 }

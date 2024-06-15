d11610e2ed74ec240f7b141023f93aed55894e67
sliverkiss <1393579810@qq.com>
2024-06-15 09:06:08 +0800

    test: 增加数据监控 v1.0.1(13)

--- routes/ping.ts
+++ routes/ping.ts
@@ -1,7 +1,7 @@
-import { Context, Response } from "file:///src/deps.ts";
+import { Request, Response } from "file:///src/deps.ts";
 import { http } from "file:///src/utils/request.ts";
 
-export async function ping(ctx: Context): Promise<Response> {
+export async function ping(req: Request): Promise<Response> {
     const data = await http.get("https://v1.hitokoto.cn/?charset=utf-8");
     return data;
 }

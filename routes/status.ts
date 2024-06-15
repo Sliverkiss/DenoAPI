475a9d9d728d342c02049f76e4d77a68f4c6bf7c
sliverkiss <1393579810@qq.com>
2024-06-15 09:06:08 +0800

    test: 增加数据监控token v1.0.1(15)

--- routes/status.ts
+++ routes/status.ts
@@ -4,7 +4,22 @@ import { http } from "file:///src/utils/request.ts";
 export async function status(req: Request): Promise<Response> {
     const params = req.url.searchParams;
     const userId = params.get("userId");
-    const data = await http.get(`https://dash.deno.com/_api/v1/organizations/${userId}/analytics`);
+    const token = params.get("token");
+    let headers = {
+        'Sec-Fetch-Dest': `empty`,
+        'Connection': `keep-alive`,
+        'Accept-Encoding': `gzip, deflate, br`,
+        'x-api-client': `true`,
+        'Sec-Fetch-Site': `same-origin`,
+        'User-Agent': `Mozilla/5.0 (iPad; CPU OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/111.0.5563.101 Mobile/15E148 Safari/604.1`,
+        'Sec-Fetch-Mode': `cors`,
+        'Cookie': `token=${token}`,
+        'Referer': `https://dash.deno.com/account/overview`,
+        'Host': `dash.deno.com`,
+        'Accept-Language': `zh-CN,zh-Hans;q=0.9`,
+        'Accept': `application/json`
+    }
+    const data = await http.get(`https://dash.deno.com/_api/v1/organizations/${userId}/analytics`,headers);
 
     // let [, ..._values] = data.values;
     // _values = sumNumbersInArrayByIndex(_values);
